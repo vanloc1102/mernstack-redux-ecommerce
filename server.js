620071743c132732961588be65e5dcf8e8fd48c6
LocNV <nguyenvanlocmmo@gmail.com>
2020-11-20 09:50:37 +0700

    uddate server.js

--- server.js
+++ server.js
@@ -37,6 +37,9 @@ readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 // port
 const port = process.env.PORT || 5000;
